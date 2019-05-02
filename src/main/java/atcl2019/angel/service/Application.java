71e537cb84dceacab4551491d46003c44ba5eaa4
3rdstage <halfface@chollian.net>
2019-05-02 21:32:23 +0900

    Building baseline for Spring Boot application with Web3j but having trouble with Spring Actuator.

--- src/main/java/atcl2019/angel/service/Application.java
+++ src/main/java/atcl2019/angel/service/Application.java
@@ -1,14 +1,38 @@
 package atcl2019.angel.service;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
 
 @SpringBootApplication
 public class Application{
+
+   private Logger logger = LoggerFactory.getLogger(this.getClass());
 
    public static void main(String... args){
 
       SpringApplication.run(Application.class, args);
    }
 
+   @Bean
+   public Web3j localGanacheCli(@Value("${ethereumNetworks.local.address}") @NotEmpty String addr,
+         @Value("${ethereumNetworks.local.port}") @Positive int port) {
+      return this.buildWeb3j("http", addr, port);
+   }
+
+
+   private Web3j buildWeb3j(@NotEmpty String protocol, @NotEmpty String addr, int port) {
+      Web3j instance = Web3j.build(new HttpService(String.format("%s://%s:%d", protocol, addr, port)));
+
+      logger.info("Ethereum connector is created for {}://{}:{}", protocol, addr, port);
+
+      return instance;
+   }
 }
