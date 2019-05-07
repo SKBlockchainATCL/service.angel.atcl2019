93861975362ab3852d69b7014e17d81c3803704c
3rdstage <halfface@chollian.net>
2019-05-08 00:28:11 +0900

    Added profile specific Spring Boot application properties.

--- scripts/ganache-cli-start.sh
+++ scripts/ganache-cli-start.sh
@@ -11,8 +11,15 @@ cd "${script_dir}"
 # Ganache CLI : https://github.com/trufflesuite/ganache-cli#using-ganache-cli
 # BIP 32 : https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
 # BIP 39 : https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
-# private key candidate
-#   - b92c249c190dd162da9d28bf3e78a730f39444a9c8ead96d6b5616ec0f25b370
+#
+# Accounts
+#   - 0xc5776c5d4ba76dd38424a160927c6b7054b55edd
+#   - 0x99322780c19b664e9902ff1031549da575de8f3b
+#   - 0xf0f0717db9387ea3b095de1ff43786c63dc93e45
+# Private keys
+#   - 0xbbd0e1d8507416b8c64e88f63b4534969b9d88e4a79ebc67f4abff122f28cfb7
+#   - 0xf8c91da1e73f5601a25cbffdac303138ffac30eeeda2680f1853b6ce325ac01b
+#   - 0x572775a6686f4b5d3b26c46133e7419e97b88b5ba1db9e0f5d3ff9a109916a47
 ganache-cli --networkId 37 \
             --port 8555 \
             --gasPrice 20000000000 \
